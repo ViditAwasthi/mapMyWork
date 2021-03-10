3b0a8744b1f262594384445967a84e79f4d9717f
Vidit Awasthi <viditawasthi2@gmail.com>
2021-03-11 03:35:30 +0530

    Added Layouts

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems =[];
 app.set("view engine", "ejs");
 
 
@@ -19,7 +20,7 @@ app.get("/", function(req, res) {
   };
   let day = today.toLocaleDateString("en-US", options);
   res.render("list", {
-    kindDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
@@ -27,11 +28,28 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+
+
 
 });
 
+app.get("/work", function(req, res){
+
+  res.render("list", {listTitle: "Work List", newListItems: workItems });
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 
 app.listen(3000, function() {
